05a9541eda903d5a6acac17488e09d86fd8464f6
Ismoil-JS <ismoilturgunpolatov1705@gmail.com>
2024-08-07 04:06:50 +0500

    feat: product table added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,11 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
-import { ProductCategoryModule } from './modules/product-category/product-category.module';
 import { BranchModule } from './modules/branch/branch.module';
+import { ProductCategoryModule } from './modules/product-category/product-category.module';
+import { ProductModule } from './modules/product/product.module';
 import { RedisModule } from './modules/redis/redis.module';
 import { UserModule } from './modules/user/user.module';
-import { ProductModule } from './modules/product/product.module';
 
 @Module({
   imports: [
