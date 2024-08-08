1191e08302cf1e1a4480f6563178c7e4ec42e2e8
Ismoil-JS <ismoilturgunpolatov1705@gmail.com>
2024-08-08 19:26:07 +0500

    feat: order is added

--- prisma/migrations/20240806231610_delivery_price/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "DeliveryPrice" (
-    "id" TEXT NOT NULL,
-    "price" INTEGER NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "DeliveryPrice_pkey" PRIMARY KEY ("id")
-);
